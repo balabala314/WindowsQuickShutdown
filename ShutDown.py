4c485a40a228183bb47ef010dc5a574f83c9e116
balabala314 <65848400+balabala314@users.noreply.github.com>
2024-06-09 08:57:44 +0800

    Update ShutDown.py

--- ShutDown.py
+++ ShutDown.py
@@ -1,17 +1,42 @@
-import wx, wx.adv, time, ctypes
+import wx, wx.adv, time, ctypes, winreg
+def getDPI():
+    HKCU = winreg.HKEY_CURRENT_USER
+    src = r"Control Panel\Desktop\WindowMetrics"
+    value = "AppliedDPI"
+    try:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, src)  # 只读
+        if key:
+            a = winreg.QueryValueEx(key, value)
+            winreg.CloseKey(key)
+    except FileNotFoundError:
+        return 1
+    return a[0]/96
 OPERATION = None
 TIME = 0
+try :
+    DPI = ctypes.windll.shcore.GetScaleFactorForMonitor(0)
+except :
+    DPI = getDPI()
+try:
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)
+except:
+    ctypes.windll.user32.SetProcessDPIAware()
 class ShutdownFrame(wx.Frame) :
     def __init__(self,parent = None, ID = wx.ID_ANY, title : str = "Quick Shutdown & Restart App") :
-        wx.Frame.__init__(self, parent, ID, size = (300,160),title = title)
+        global DPI
+        wx.Frame.__init__(self, parent, ID, size = (round(300 * DPI),round(160 * DPI)), title = title)
         self.SetWindowStyleFlag(wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
-        
+        try :
+            icon = wx.Icon('power.ico', wx.BITMAP_TYPE_ICO)
+            self.SetIcon(icon)
+        except :
+            pass
         panel = wx.Panel(self, )
         self.MainSizer = wx.BoxSizer(wx.VERTICAL)
         self.ButtonSizer = wx.BoxSizer(wx.HORIZONTAL)
         self.TimeSizer = wx.BoxSizer(wx.HORIZONTAL)
         
-        self.LabelFont = wx.Font(18, wx.DEFAULT, wx.FONTSTYLE_NORMAL, wx.BOLD)
+        self.LabelFont = wx.Font(18, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Microsoft YaHei")
         self.LabelText = wx.StaticText(panel, label = "Quick to Shutdown", style = wx.ALIGN_CENTRE_HORIZONTAL)
         self.LabelText.SetFont(self.LabelFont)
         
@@ -28,27 +53,25 @@ class ShutdownFrame(wx.Frame) :
         self.TimePicker.Enable(False)
         self.TimePicker.SetTime(0,0,0)
         
-        self.MainSizer.Add(self.LabelText, 3, wx.ALL|wx.EXPAND, 5)
-        self.ButtonSizer.Add(self.ButtonShut, 1, wx.ALL|wx.EXPAND, 5)
-        self.ButtonSizer.Add(self.ButtonRest, 1, wx.ALL|wx.EXPAND, 5)
-        self.ButtonSizer.Add(self.ButtonQuit, 1, wx.ALL|wx.EXPAND, 5)
+        self.MainSizer.Add(self.LabelText, 3, wx.ALL|wx.EXPAND, round(5 * DPI))
+        self.ButtonSizer.Add(self.ButtonShut, 1, wx.ALL|wx.EXPAND, round(5 * DPI))
+        self.ButtonSizer.Add(self.ButtonRest, 1, wx.ALL|wx.EXPAND, round(5 * DPI))
+        self.ButtonSizer.Add(self.ButtonQuit, 1, wx.ALL|wx.EXPAND, round(5 * DPI))
         
-        self.TimeSizer.Add(self.TimeChoose, 1,wx.ALL|wx.EXPAND, 5)
-        self.TimeSizer.Add(self.TimePicker, 1,wx.ALL|wx.EXPAND, 5)
+        self.TimeSizer.Add(self.TimeChoose, 1,wx.ALL|wx.EXPAND, round(5 * DPI))
+        self.TimeSizer.Add(self.TimePicker, 1,wx.ALL|wx.EXPAND, round(5 * DPI))
         
-        self.MainSizer.Add(self.ButtonSizer, 2, wx.ALL|wx.EXPAND, 5)
+        self.MainSizer.Add(self.ButtonSizer, 2, wx.ALL|wx.EXPAND, round(5 * DPI))
         self.MainSizer.Add(self.TimeSizer, 2, wx.ALL|wx.EXPAND)
         panel.SetSizer(self.MainSizer)
     def Shutdown(self, event) :
         global OPERATION, TIME
-        print("Shutdown!!", event)
         OPERATION = "shutdown"
         if self.TimeChoose.IsChecked() :
             TIME = self.TimePicker.GetTime()[0] * 3600 + self.TimePicker.GetTime()[1] * 60 + self.TimePicker.GetTime()[2]
         self.Close(True)
     def Restart(self, event) :
         global OPERATION, TIME
-        print("Restart!!", event)
         OPERATION = "restart"
         if self.TimeChoose.IsChecked() :
             TIME = self.TimePicker.GetTime()[0] * 3600 + self.TimePicker.GetTime()[1] * 60 + self.TimePicker.GetTime()[2]
@@ -57,7 +80,6 @@ class ShutdownFrame(wx.Frame) :
         global OPERATION, TIME
         self.Close(True)
     def CheckBoxHandle(self, event) :
-        print("CheckBox Event :", self.TimeChoose.IsChecked())
         self.TimePicker.Enable(self.TimeChoose.IsChecked())
 def main() :
     app = wx.App(False)

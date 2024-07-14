f77b6e278fbe67757f90b83641bfd372527108b5
balabala314 <65848400+balabala314@users.noreply.github.com>
2024-07-14 14:23:13 +0800

    Update ShutDown.py

--- ShutDown.py
+++ ShutDown.py
@@ -4,7 +4,7 @@ def getDPI():
     src = r"Control Panel\Desktop\WindowMetrics"
     value = "AppliedDPI"
     try:
-        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, src)  # readonly
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, src)  # 只读
         if key:
             a = winreg.QueryValueEx(key, value)
             winreg.CloseKey(key)
@@ -25,7 +25,8 @@ class ShutdownFrame(wx.Frame) :
     def __init__(self,parent = None, ID = wx.ID_ANY, title : str = "Quick Shutdown & Restart App") :
         global DPI
         wx.Frame.__init__(self, parent, ID, size = (round(300 * DPI),round(160 * DPI)), title = title)
-        self.SetWindowStyleFlag(wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+        self.Bind(wx.EVT_CLOSE, self.Close)
+        self.SetWindowStyleFlag(wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX) | wx.STAY_ON_TOP)
         try :
             icon = wx.Icon('power.ico', wx.BITMAP_TYPE_ICO)
             self.SetIcon(icon)
@@ -35,32 +36,32 @@ class ShutdownFrame(wx.Frame) :
         self.MainSizer = wx.BoxSizer(wx.VERTICAL)
         self.ButtonSizer = wx.BoxSizer(wx.HORIZONTAL)
         self.TimeSizer = wx.BoxSizer(wx.HORIZONTAL)
-        
+
         self.LabelFont = wx.Font(18, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Microsoft YaHei")
         self.LabelText = wx.StaticText(panel, label = "Quick to Shutdown", style = wx.ALIGN_CENTRE_HORIZONTAL)
         self.LabelText.SetFont(self.LabelFont)
-        
+
         self.ButtonShut = wx.Button(panel, wx.ID_ANY, label = "&Shutdown")
         self.Bind(wx.EVT_BUTTON, self.Shutdown, self.ButtonShut)
         self.ButtonRest = wx.Button(panel, wx.ID_ANY, label = "&Restart")
         self.Bind(wx.EVT_BUTTON, self.Restart, self.ButtonRest)
         self.ButtonQuit = wx.Button(panel, wx.ID_EXIT, label = "&Quit")
-        self.Bind(wx.EVT_BUTTON, self.Quit, self.ButtonQuit)
-        
+        self.Bind(wx.EVT_BUTTON, self.Close, self.ButtonQuit)
+
         self.TimeChoose = wx.CheckBox(panel, label="Set Time :")
         self.Bind(wx.EVT_CHECKBOX, self.CheckBoxHandle, self.TimeChoose)
         self.TimePicker = wx.adv.TimePickerCtrl(panel)
         self.TimePicker.Enable(False)
         self.TimePicker.SetTime(0,0,0)
-        
+
         self.MainSizer.Add(self.LabelText, 3, wx.ALL|wx.EXPAND, round(5 * DPI))
         self.ButtonSizer.Add(self.ButtonShut, 1, wx.ALL|wx.EXPAND, round(5 * DPI))
         self.ButtonSizer.Add(self.ButtonRest, 1, wx.ALL|wx.EXPAND, round(5 * DPI))
         self.ButtonSizer.Add(self.ButtonQuit, 1, wx.ALL|wx.EXPAND, round(5 * DPI))
-        
+
         self.TimeSizer.Add(self.TimeChoose, 1,wx.ALL|wx.EXPAND, round(5 * DPI))
         self.TimeSizer.Add(self.TimePicker, 1,wx.ALL|wx.EXPAND, round(5 * DPI))
-        
+
         self.MainSizer.Add(self.ButtonSizer, 2, wx.ALL|wx.EXPAND, round(5 * DPI))
         self.MainSizer.Add(self.TimeSizer, 2, wx.ALL|wx.EXPAND)
         panel.SetSizer(self.MainSizer)
@@ -76,11 +77,11 @@ class ShutdownFrame(wx.Frame) :
         if self.TimeChoose.IsChecked() :
             TIME = self.TimePicker.GetTime()[0] * 3600 + self.TimePicker.GetTime()[1] * 60 + self.TimePicker.GetTime()[2]
         self.Close(True)
-    def Quit(self, event) :
-        global OPERATION, TIME
-        self.Close(True)
     def CheckBoxHandle(self, event) :
         self.TimePicker.Enable(self.TimeChoose.IsChecked())
+    def Close(self, force = True) :
+        self.Show(False)
+        self.Destroy()
 def main() :
     app = wx.App(False)
     SDFrame = ShutdownFrame()
